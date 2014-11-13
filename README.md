1512fc17631e529e457201cbb50709b8e482d5d1
crazyyi <gdzack@gmail.com>
2014-11-13 20:57:22 +0800

    Changed application to use uwsgi and fixed all bugs.

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Simple Course is the next generation course ~ teacher ~ student management system. 
 
+## Deployment
+
+*	Need to change hostname to 121.42.30.115 before deployment
+
 ## Main features include:
 
 *	Creating, editing and deleting courses, teachers and students.
--- index.py
+++ index.py
@@ -1,24 +1,28 @@
 import os, sys, bottle
+from bottle import debug, route, request, run, Bottle, static_file
+from apps import model, views
 from beaker.middleware import SessionMiddleware
-from bottle import debug, route, run, default_app
 
-from app import model, views
+app = bottle.app()
 
-@bottle.route('/assets/<filepath:path>')
-def server_static(filepath):
-    return bottle.static_file(filepath, root='assets')
-
-# Boot
-if __name__ == '__main__':
-	debug(True)
-
-	app = default_app()
-
-	session_options = {
+session_options = {
 		'session.type': 'file',
 		'session.cookie_expires': 300,
 		'session.data_dir': './data',
 		'session.auto': True
 	}
-	app = SessionMiddleware(app, session_options)
-	run(host='localhost', port=8080, reloader=True, app=app)
+
+app_session = SessionMiddleware(app, session_options)
+
+@app.route('/assets/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='assets')
+
+@app.route('/test')
+def test():
+    s = request.environ.get('beaker.session')
+    s['test'] = s.get('test', 0) + 1
+    s.save()
+    return 'Test counter: %d' % s['test']
+    
+run(app=app_session)
